879b0baf9af1bb32f1ae9b5c32a3dab388a9bd45
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-07-24 18:27:44 -0300

    Added MLE

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from KDE import kde
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-DATA_SIZE = 10000
-X_AMOUNT = 50
 
+DATA_SIZE = 1000
+X_AMOUNT = 50
+H_AMOUNT = 50
 data = np.random.randn(DATA_SIZE).tolist()
-h = 1.06*np.std(data)*(DATA_SIZE**-0.2)
+h = np.linspace(0.1, 10, H_AMOUNT)
 x_result = np.linspace(min(data), max(data), X_AMOUNT)
-y_result = []
+auxVar = [None] * X_AMOUNT
+mle = []
 tempoi = time.time()
-for i in range(x_result.shape[0]):
-    y_result.append(kde(x_result[i], data, h))
+for i in range(h.shape[0]):
+    for j in range(x_result.shape[0]):
+        auxVar[j] = np.log(kde(x_result[j], data, h[i]))
+    mle.append(np.sum(auxVar))
 tempototal = time.time() - tempoi
-print(tempototal)
+print("Time: "+str(tempototal)+"s")
+maxMLE = max(mle)
+best = mle.index(maxMLE)
+print("The best \'h\' is "+str(h[best])+", witch gives the MLE value of "+str(maxMLE)+".")
 plt.figure(1)
-plt.hist(data, bins=X_AMOUNT, density=True)
-plt.plot(x_result, y_result, color='red')
+plt.plot(h, mle, color='red')
+plt.ylabel('Maximum Likelihood Estimation (MLE)')
+plt.xlabel('\'h\' parameter')
 plt.show()
