7c4e92016bfe71c780807448cccc49dbebfd810d
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-08-01 08:35:00 -0300

    Added different distributions

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import time
 
 DATA_SIZE = 1000
-X_AMOUNT = 50
+X_AMOUNT = 500
 H_AMOUNT = 100
 data = np.random.randn(DATA_SIZE).tolist()
-h = np.linspace(0.01, 5, H_AMOUNT)
+#data = np.random.exponential(1.0, X_AMOUNT).tolist()
+#data = np.random.laplace(0.0, 1.0, X_AMOUNT).tolist()
+h = np.linspace(0.01, 2, H_AMOUNT)
 x_result = np.random.rand(X_AMOUNT)
 x_result = np.sort(x_result)
 auxVar = [None] * X_AMOUNT
@@ -29,12 +31,16 @@ plt.xlabel('\'h\' parameter')
 plt.show()
 
 best_h = h[best]
-print(best_h)
-x_result = (x_result*6)-3
-y_result = []
-for i in range(x_result.shape[0]):
-    y_result.append(kde(x_result[i], data, best_h))
-plt.figure(2)
-plt.hist(data, bins=X_AMOUNT, density=True)
-plt.plot(x_result, y_result, color='red')
+print("h: "+str(best_h))
+#x = (x*6)-3  #normal
+#x = (x*5)-0  #exponencial
+#x = (x*16)-8 #laplace
+#x = (x*max(data)*2)-max(data)
+xx = np.linspace(min(data), max(data), X_AMOUNT)
+kde_result = []
+for i in range(xx.shape[0]):
+    kde_result.append(kde(xx[i], data, best_h))
+plt.figure(1)
+plt.hist(data, bins=25, density=True)
+plt.plot(xx, kde_result, color='red')
 plt.show() 
