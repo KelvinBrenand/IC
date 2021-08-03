7dc6e1c294812cd274ab78639995b964f4a96b87
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-08-03 10:25:10 -0300

    Added nrm

--- functions.py
+++ functions.py
@@ -17,3 +17,33 @@ def mle(h, x, data):
             auxVar[j] = np.log(kde(x[j], data, h[i]))
         mle.append(np.sum(auxVar))
     return mle
+
+def nrm_numerator(x, data, h):
+    result = []
+    for i in range(x.shape[0]):
+        top = []
+        bottom = []
+        for j in range(len(data)):
+            varAux = gaussian((x[i] - data[j])/h)
+            bottom.append(varAux)
+            top.append(varAux*(x[i] - data[j])*(x[i] - data[j]))
+        result.append(sum(top)/(sum(bottom)*h*h*h))
+    return sum(result)-(x.shape[0]/h)
+def nrm_denominator(x, data, h):
+    result = []
+    for i in range(x.shape[0]):
+        top = []
+        bottom = []
+        for j in range(len(data)):
+            varAux = gaussian((x[i] - data[j])/h)
+            bottom.append(varAux)
+            top.append(varAux*(x[i] - data[j])*(x[i] - data[j]))
+        result.append((sum(top)*-3)/(sum(bottom)*h*h*h*h))
+    return sum(result)-(x.shape[0]/(h*h))
+def nrm(x, data, h):
+    best_h = h
+    frac = nrm_numerator(x, data, best_h)/nrm_denominator(x, data, best_h)
+    while abs(frac) >= 0.0001:
+        frac = nrm_numerator(x, data, best_h)/nrm_denominator(x, data, best_h)
+        best_h = best_h - frac
+    return best_h
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from functions import kde, mle
+from functions import kde, mle, nrm
 import numpy as np
 import matplotlib.pyplot as plt
 
 DATA_SIZE = 1000
-X_AMOUNT = 250
+X_AMOUNT = 50
 H_AMOUNT = 100
-#data = np.random.randn(DATA_SIZE).tolist()
-data = np.random.exponential(1.0, X_AMOUNT).tolist()
+data = np.random.randn(DATA_SIZE).tolist()
+#data = np.random.exponential(1.0, X_AMOUNT).tolist()
 #data = np.random.laplace(0.0, 1.0, X_AMOUNT).tolist()
-h = np.linspace(0.01, 2, H_AMOUNT)
+#h = np.linspace(0.01, 2, H_AMOUNT)
 x = np.random.rand(X_AMOUNT)
 x = np.sort(x)
 
-mle_result = mle(h, x, data)
-best = mle_result.index(max(mle_result))
-print("The best \'h\' is "+str(h[best])+".")
-plt.figure(1)
-plt.plot(h, mle_result, color='red')
-plt.ylabel('Maximum Likelihood Estimation (MLE)')
-plt.xlabel('\'h\' parameter')
-plt.show()
+# mle_result = mle(h, x, data)
+# best = mle_result.index(max(mle_result))
+# print("The best \'h\' is "+str(h[best])+".")
+# plt.figure(1)
+# plt.plot(h, mle_result, color='red')
+# plt.ylabel('Maximum Likelihood Estimation (MLE)')
+# plt.xlabel('\'h\' parameter')
+# plt.show()
 
-best_h = h[best]
+
+#best_h = h[best]
+best_h = nrm(x, data, 2.0)
+print("The best \'h\' is: "+str(best_h)+".")
 xx = np.linspace(min(data), max(data), X_AMOUNT)
 kde_result = []
 for i in range(xx.shape[0]):
