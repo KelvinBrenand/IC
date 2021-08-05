f316f3d8eb4b4e798fd044f8c62ea26878bf45ce
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-08-05 10:48:35 -0300

    Improved the nrm function

--- functions.py
+++ functions.py
@@ -18,32 +18,27 @@ def mle(h, x, data):
         mle.append(np.sum(auxVar))
     return mle
 
-def nrm_numerator(x, data, h):
-    result = []
+def frac_result(x, data, h):
+    resultFirstDer = []
+    resultSecDer = []
     for i in range(x.shape[0]):
         top = []
         bottom = []
         for j in range(len(data)):
             varAux = gaussian((x[i] - data[j])/h)
-            bottom.append(varAux)
             top.append(varAux*(x[i] - data[j])*(x[i] - data[j]))
-        result.append(sum(top)/(sum(bottom)*h*h*h))
-    return sum(result)-(x.shape[0]/h)
-def nrm_denominator(x, data, h):
-    result = []
-    for i in range(x.shape[0]):
-        top = []
-        bottom = []
-        for j in range(len(data)):
-            varAux = gaussian((x[i] - data[j])/h)
             bottom.append(varAux)
-            top.append(varAux*(x[i] - data[j])*(x[i] - data[j]))
-        result.append((sum(top)*-3)/(sum(bottom)*h*h*h*h))
-    return sum(result)-(x.shape[0]/(h*h))
+        sumTop = sum(top)
+        sumBottom = sum(bottom)
+        resultFirstDer.append(sumTop/(sumBottom*h*h*h))
+        resultSecDer.append((sumTop*-3)/(sumBottom*h*h*h*h))
+    return sum(resultFirstDer)-(x.shape[0]/h), sum(resultSecDer)-(x.shape[0]/(h*h))
 def nrm(x, data, h):
     best_h = h
-    frac = nrm_numerator(x, data, best_h)/nrm_denominator(x, data, best_h)
-    while abs(frac) >= 0.0001:
-        frac = nrm_numerator(x, data, best_h)/nrm_denominator(x, data, best_h)
+    funcReturn = frac_result(x, data, best_h)
+    frac = funcReturn[0]/funcReturn[1]
+    while abs(frac) >= 0.001:
+        funcReturn = frac_result(x, data, best_h)
+        frac = funcReturn[0]/funcReturn[1]
         best_h = best_h - frac
     return best_h
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from functions import kde, mle, nrm
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 DATA_SIZE = 1000
 X_AMOUNT = 50
 H_AMOUNT = 100
 data = np.random.randn(DATA_SIZE).tolist()
-#data = np.random.exponential(1.0, X_AMOUNT).tolist()
-#data = np.random.laplace(0.0, 1.0, X_AMOUNT).tolist()
+#data = np.random.exponential(1.0, DATA_SIZE).tolist()
+#data = np.random.laplace(0.0, 1.0, DATA_SIZE).tolist()
 #h = np.linspace(0.01, 2, H_AMOUNT)
 x = np.random.rand(X_AMOUNT)
 x = np.sort(x)
@@ -23,8 +24,11 @@ x = np.sort(x)
 
 
 #best_h = h[best]
-best_h = nrm(x, data, 2.0)
+tempoi = time.time()
+best_h = nrm(x, data, 1.0)
+tempototal = time.time() - tempoi
 print("The best \'h\' is: "+str(best_h)+".")
+print("Time: "+str(tempototal)+"s.")
 xx = np.linspace(min(data), max(data), X_AMOUNT)
 kde_result = []
 for i in range(xx.shape[0]):
@@ -32,4 +36,5 @@ for i in range(xx.shape[0]):
 plt.figure(2)
 plt.hist(data, bins=25, density=True)
 plt.plot(xx, kde_result, color='red')
+plt.title("Newton-Raphson Method\nInitial \'h\' = 1. Best \'h\' = "+str(best_h))
 plt.show()
