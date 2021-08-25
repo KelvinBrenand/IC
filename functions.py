edc9d24557f3b1a05b6a772a07fc4d3760565325
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-08-25 13:35:18 -0300

    Added Leave one out NRM method and the multivariate kde, i guess

--- functions.py
+++ functions.py
@@ -49,12 +49,49 @@ class functions(object):
             best_h = best_h - frac
         print(best_h)
         return best_h
+    ######################### LOO ####################################
+    def __frac_resultloo(self, data, h):
+        resultFirstDer = []
+        resultSecDer = []
+        databkp = data.copy()
+        for i in range(len(data)):
+            top = []
+            bottom = []
+            element = databkp[i]
+            databkp.pop(i)
+            for j in range(len(databkp)):
+                varAux = self.__gaussian((element - databkp[j])/h)
+                top.append(varAux*(element - databkp[j])*(element - databkp[j]))
+                bottom.append(varAux)
+            databkp.clear()
+            databkp = data.copy()
+            sumTop = sum(top)
+            sumBottom = sum(bottom)
+            resultFirstDer.append(sumTop/(sumBottom*h*h*h))
+            resultSecDer.append((sumTop*-3)/(sumBottom*h*h*h*h))
+        return sum(resultFirstDer)-(len(data)/h), sum(resultSecDer)+(len(data)/(h*h))
+    def nrmloo(self,data, h):
+        best_h = h
+        funcReturn = self.__frac_resultloo(data, best_h)
+        frac = funcReturn[0]/funcReturn[1]
+        while abs(frac) >= 0.01:
+            print(best_h)
+            funcReturn = self.__frac_resultloo(data, best_h)
+            frac = funcReturn[0]/funcReturn[1]
+            best_h = best_h - frac
+        print(best_h)
+        return best_h
+
     ######################### D DIMENSIONAL ##########################
-    def __kernelNormal(self, x, H):
-        return ((2*math.pi)**(-len(x)/2))*((np.linalg.det(H))**(-0.5))*math.exp(-0.5*H.transpose()*np.linalg.inv(H)*x)
-    def mkde(self, x, array, H):
+    def __kernelNormal(self, x):
+        H = np.array([[1.0, 0.0],[0.0, 1.0]])
+        a = ((2*math.pi)**(-x.ndim/2))
+        b = ((np.linalg.det(H))**(-0.5))
+        c = np.exp(-0.5*x.transpose()*np.linalg.inv(H)*x)
+        return a*b*c
+    def mkde(self, x, array, h):
         N = len(array)
         sum = 0
         for i in range(N):
-            sum += self.__kernelNormal(self, (x - array[i])/H, H)
-        return sum/(N*H)
+            sum += self.__kernelNormal((x - array[i])/h)
+        return sum/(N*h**x.ndim)
