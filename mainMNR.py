bca4bcf004f3a92a7a765ab81811803d49746383
Kelvin Brenand <brenand.kelvin@gmail.com>
2021-09-06 16:26:06 -0300

    Minor changes

--- mainMNR.py
+++ mainMNR.py
@@ -1,14 +1,13 @@
 from functions import functions
 from matplotlib import pyplot as plt
 import numpy as np
-np.random.seed(seed=123)
-
-obj = functions()
 DATA_SIZE = 250
+np.random.seed(seed=123)
+obj = functions()
+
 mean = [0, 0]
 cov = [[1, 0], [0, 1]]
 data = np.random.multivariate_normal(mean, cov, DATA_SIZE)
-print(data.shape)
 best_h = obj.multidimentionalNewtonRaphson(data, 1.0)
 kde_result = obj.multidimensionalKernelDensityEstimation(data, best_h)
 print(np.array(kde_result).shape)
