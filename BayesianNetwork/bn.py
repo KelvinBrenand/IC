9b650a878e31ea1f4ac2e47ecad16241f9b48931
KelvinBrenand <brenand.kelvin@gmail.com>
2022-05-16 17:19:41 -0300

    MLE fixed

--- BayesianNetwork/bn.py
+++ BayesianNetwork/bn.py
@@ -337,16 +337,16 @@ class newtonRapson(object):
             sum += self.__multivariateGaussian(self.__listDivision(self.__listSubtraction(x, data[i]), h))
         return (sum/(len(data)*h**self.__ndim(x)))
 
-    def maximumLikelihoodEstimation(self, data, h):
+    def maximumLikelihoodEstimation(self, data, h): #virou uma função de fazer Leave-One-Out, basicamente.
         auxVar = [None] * len(data)
         databkp = data.copy()
         for i in range(len(data)):
             element = databkp[i]
             databkp.pop(i)
             if isinstance(element, float):
-                auxVar[i] = math.log(self.kernelDensityEstimation(element, databkp, h))
+                auxVar[i] = self.kernelDensityEstimation(element, databkp, h)
             else:
-                auxVar[i] = math.log(self.multivariateKernelDensityEstimation(element, databkp, h))
+                auxVar[i] = self.multivariateKernelDensityEstimation(element, databkp, h)
             databkp.clear()
             databkp = data.copy()
-        return (sum(auxVar))
+        return auxVar
--- BayesianNetwork/bnMain.py
+++ BayesianNetwork/bnMain.py
@@ -1,3 +1,4 @@
+import math
 from sklearn import datasets
 from bn import newtonRapson
 import numpy as np
@@ -19,13 +20,28 @@ X1_mle = obj.maximumLikelihoodEstimation(X1, X1_h)
 
 X2_mle = obj.maximumLikelihoodEstimation(X2, X2_h)
 
-print("MLE Independente: "+str(round(X1_mle*X2_mle, 2)))
+X1_mle = np.array(X1_mle)
+X2_mle = np.array(X2_mle)
+X3 = X1_mle*X2_mle
+X3 = np.log(X3)
+X3 = sum(X3)
+print("MLE Independente: "+str(round(X3, 2)))
 
 X12 = np.column_stack((np.array(X1),np.array(X2))).tolist()
 X12_h = obj.multivariateNewtonRaphson(X12, 0.9)
 
-X12_mle = obj.maximumLikelihoodEstimation(X12, X12_h)/X2_mle
-print("MLE 'A | B': "+str(round(X12_mle, 2)))
+X12_mle = obj.maximumLikelihoodEstimation(X12, X12_h)
+X12_mle = np.array(X12_mle)
+XX12 = X12_mle/X2_mle
+XX12 = np.log(XX12)
+XX12 = sum(XX12)
 
-X21_mle = obj.maximumLikelihoodEstimation(X12, X12_h)/X1_mle
-print("MLE 'B | A': "+str(round(X21_mle, 2)))
+print("MLE 'A | B': "+str(round(XX12, 2)))
+
+X21_mle = X12_mle
+XX21 = X21_mle/X1_mle
+XX21 = np.log(XX21)
+XX21 = sum(XX21)
+
+
+print("MLE 'B | A': "+str(round(XX21, 2)))
