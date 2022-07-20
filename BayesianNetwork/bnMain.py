a91b5f1fc7936f437101be6093cba17fd2dec8d5
KelvinBrenand <brenand.kelvin@gmail.com>
2022-07-20 15:30:47 -0300

    One bn for each iris class

--- BayesianNetwork/bnMain.py
+++ BayesianNetwork/bnMain.py
@@ -2,7 +2,16 @@ from sklearn import datasets
 from bn import bayesianNetwork
 import numpy as np
 
-data = datasets.load_iris().data[:,:4]
 obj = bayesianNetwork()
-print("Input shape: ",data.shape)
-print(np.array(obj.MLE(data.tolist())))
+#data = datasets.load_iris().data[:,:4]
+#print("Input shape: ",data.shape)
+#print(np.array(obj.MLE(data.tolist())))
+
+data1 = datasets.load_iris().data[:50,:]
+print(np.array(obj.MLE(data1.tolist())))
+
+data1 = datasets.load_iris().data[50:100,:]
+print(np.array(obj.MLE(data1.tolist())))
+
+data1 = datasets.load_iris().data[100:,:]
+print(np.array(obj.MLE(data1.tolist())))
